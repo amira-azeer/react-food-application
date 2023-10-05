7c7e038efa04e846fcb653c299201f05ea817b48
Amira Azeer <amira@linearsix.com>
2023-10-05 10:06:58 +0530

    RTK Queries Implemented

--- src/components/layout/meal-components/Meals.jsx
+++ src/components/layout/meal-components/Meals.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react'
-import { mealsData } from '../../../data/Data'
+import React from 'react'
 import MealsCard from './MealsCard'
 import { Box, useMediaQuery } from '@mui/material'
 
+// import { mealsData } from '../../../data/Data'
 // import { getMeals } from '../../../shared/api-calls'
 // import axios from 'axios';
-// import { useGetMealsQuery } from '../../../shared/RTK-queries'
+import { useGetMealsQuery } from '../../../shared/RTK-queries'
 
 const Meals = () => {
     const isNonMobile = useMediaQuery("(min-width: 1000px)"); 
-    // let data;
-    // const { data, isLoading } = useGetMealsQuery()
 
-    // useEffect(() => {
-    //   data = getMeals()
-    //   console.log(data)
-    // }, [ data, ])
+    let { data, isLoading, } = useGetMealsQuery()
+
+    const componentArray = []
+
+    for(let key in data){
+      componentArray.push(<MealsCard key={key} id={key} title={data[key].name} src={data[key].src} price={data[key].price}></MealsCard>)
+    }
+
+
 
     
   return (
@@ -29,13 +32,15 @@ const Meals = () => {
         sx={{ "& > div": { gridColumn: isNonMobile ? undefined : "span 4" }, // Span 4 takes up the entire width
         }}>
         {
-            mealsData.map(e =>  
-                <MealsCard key={e.id} id={e.id} title={e.name} src={e.src} price={e.price}></MealsCard>   
-            ) 
+            // mealsData.map(e =>  
+            //     <MealsCard key={e.id} id={e.id} title={e.name} src={e.src} price={e.price}></MealsCard>   
+            // ) 
 
           //   data ? <h1>Loading...</h1>  : data.map(e =>  
           //     <MealsCard key={e.id} id={e.id} title={e.name} src={e.src} price={e.price}></MealsCard>   
           // ) 
+
+          isLoading ? <h1>Loading....</h1> : componentArray
         }
     </Box>
   )
